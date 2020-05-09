4be6284f4aba8f628196ec23e075586b9f80ff7b
Aashupokemon <ayushguptavector@gmail.com>
2020-05-09 10:07:10 +0530

    b commit

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
 <?
-   # This is a comment, 
-   # This is the second line of the comment
-   
-   // This is a comment too. Each style comments only
-   print "An example with single line comments";
+print "hello";
 ?>
